f67b8bc58ce670ce0cd316741382828e1d9c8690
wyang <wyangxp@qq.com>
2018-12-18 11:15:20 +0800

    update

--- job-common/Common.php
+++ job-common/Common.php
@@ -15,6 +15,6 @@ class Common
     public function put($obj)
     {
         $pheanstalk = Pheanstalk::create('127.0.0.1');
-        $pheanstalk->useTube('demo')->put($obj);
+        return $pheanstalk->useTube('demo')->put($obj);
     }
 }
